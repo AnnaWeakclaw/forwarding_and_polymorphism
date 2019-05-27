6fb0a19fa81416f1e38cb7a93567b250d1401fb6
Anna <annas.wieclaw@gmail.com>
2019-05-27 19:42:18 +0100

    Refactor ScrambledDiary to use polymorphism and have few classes with the same method name

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Forwarding and Polymorphism
-This is a better way of organising your code in Object Oriented Programming
+This is a better way of organising your code in Object Oriented Programming.
+Forwarding - do a check first and then call your new user class.
+
+### If we're adding code we're not changing existing code — so we can't be adding any new bugs to our existing classes.
+Polymorphism - all the classes with compare method doing different comparisons.
---
+++ scrambledRefactored.rb
@@ -0,0 +1,47 @@
+# Use a polymorphic relationship to extract the different scrambling behaviours from ScrambledDiary.
+class ScrambledDiary
+  def initialize(contents)
+    @contents = contents
+  end
+
+  def read
+    @contents
+  end
+
+  def way_of_scabmbling(way)
+    way.scramble_by
+  end
+end
+
+class Reversing
+  def initialize(contents)
+    @contents = contents
+  end
+
+  def scramble_by
+    @contents = @contents.reverse
+  end
+end
+
+class Advancing
+  def initialize(contents, steps)
+    @contents = contents
+    @steps = steps
+  end
+
+  def scramble_by
+    scrambled_or_plain_chars = @contents.chars
+    plain_or_scrambled_chars = scrambled_chars.map do |char|
+      (char.ord - @steps).chr
+    end
+    @contents = plain_or_scrambled_chars.join
+  end
+end
+
+class Tests
+  def run
+    ScrambledDiary.new("hello lala").way_of_scabmbling(Advancing.new("hello lala", -2))
+    ScrambledDiary.new("hello lala").way_of_scabmbling(Reversing("hello lala"))
+    ScrambledDiary.new("hello lala").way_of_scabmbling(Advancing.new("hello lala", 2))
+  end
+end
